e6c16ddd6e8fc8ffcbf85afa560a21d1b9cc7c06
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-06-17 14:50:22 +0300

    practiced working with form, did primitive calculator

--- js/js_main5.js
+++ js/js_main5.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+$('#formSum').on('submit', function (event) {
+     event.preventDefault();  //  отменяем отправку формы
 
+    let numberOne, numberTwo, summ;
+      numberOne = $('#numberOne').val(); // присваиваем переменным значение инпутов
+      numberTwo = $('#numberTwo').val();
+      numberOne = parseInt(numberOne); //по дефолту значение из инпутов это строка
+      numberTwo = parseInt(numberTwo); // этим методом преобразуем их в число
 
+    if (isNaN(numberOne) || !Number(numberOne)) numberOne = 0; // проверки на отсутствие числа
+    if (isNaN(numberTwo) || !Number(numberTwo)) numberTwo = 0;
+
+    summ = numberOne + numberTwo
+      $('#sumResult').text(summ); // выводим результат
+});
 });
